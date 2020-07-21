b434cb738373adcaa1955ac7026723422980625a
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-21 03:42:11 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
   if(user_guess == random_number)
      puts 'You guessed the correct number!'
   elsif(user_guess != random_number && user_guess != 'exit')
-     puts "Sorry! The computer guessed #{random_number}"
+     puts "Sorry! The computer guessed #{random_number}."
   elsif (user_guess == 'exit')
    puts 'Goodbye!'
  end 
